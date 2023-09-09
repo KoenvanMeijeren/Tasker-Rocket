49d8f63a3a856c4e44c2515df04ae5a130bf40be
koenvanmeijeren <koenvanmeijeren@develto.nl>
2023-09-09 11:55:53 +0200

    Add support viewing sourcecode

--- src/components/project/TaskView.tsx
+++ src/components/project/TaskView.tsx
@@ -1,14 +1,69 @@
 import { GitHubTreeItem } from '@/lib/repository/gitHubData';
+import CodeMirror, { basicSetup } from '@uiw/react-codemirror';
+import { githubDark, githubLight } from '@uiw/codemirror-theme-github';
+import { Box, useColorModeValue } from '@chakra-ui/react';
+import { javascript } from '@codemirror/lang-javascript';
+import { json } from '@codemirror/lang-json';
+import { useMemo, useState } from 'react';
+import { css } from '@codemirror/lang-css';
+import { html } from '@codemirror/lang-html';
 import { Markdown } from '@/components/markdown/Markdown';
+import { LanguageSupport } from '@codemirror/language';
 
 export default function TaskView(params: { item: GitHubTreeItem }) {
-	const fileContent = Buffer.from(params.item.content ?? '', 'base64').toString(
-		'utf8'
-	);
+	const [fileContent, setFileContent] = useState('');
+	const [extension, setExtension] = useState<LanguageSupport[]>([]);
+	const fileExtension = params.item.url.split('.').pop()?.split('?')[0] ?? '';
+
+	useMemo(() => {
+		setFileContent(
+			Buffer.from(params.item.content ?? '', 'base64').toString('utf8')
+		);
+		switch (fileExtension) {
+			case 'json':
+				setExtension([json()]);
+				break;
+			case 'js':
+				setExtension([javascript()]);
+				break;
+			case 'css':
+				setExtension([css()]);
+				break;
+			case 'html':
+				setExtension([html()]);
+				break;
+			case 'ts':
+				setExtension([
+					javascript({
+						typescript: true,
+					}),
+				]);
+				break;
+			case 'tsx':
+				setExtension([
+					javascript({
+						jsx: true,
+						typescript: true,
+					}),
+				]);
+				break;
+		}
+	}, [params]);
+
+	const codeMirrorTheme = useColorModeValue(githubLight, githubDark);
+
+	if (fileExtension === 'md') {
+		return <Markdown markdown={fileContent}></Markdown>;
+	}
 
 	return (
-		<>
-			<Markdown markdown={fileContent}></Markdown>
-		</>
+		<Box minWidth="200px" maxWidth="980px" m="auto">
+			<CodeMirror
+				theme={codeMirrorTheme}
+				editable={false}
+				value={fileContent}
+				extensions={extension}
+			/>
+		</Box>
 	);
 }

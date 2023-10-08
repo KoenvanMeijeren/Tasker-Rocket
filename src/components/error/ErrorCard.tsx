1f1b2cef25613313989b6059857ba667092a19f9
FOXTetsuo <60584659+FOXTetsuo@users.noreply.github.com>
2023-10-08 18:42:41 +0200

    added button css

--- src/components/error/ErrorCard.tsx
+++ src/components/error/ErrorCard.tsx
@@ -1,7 +1,18 @@
 import React from 'react';
+import './errorCard.css';
 
 function onDismiss() {
 	window.location.reload();
+}
+
+function dismiss() {
+	// delete this element from the DOM
+	const element = document.querySelector('.error-card');
+	if (element) {
+		element.remove();
+	}
+	// delay for a second and run onDismiss
+	onDismiss();
 }
 
 function ErrorCard(error: string): JSX.Element {
@@ -9,8 +20,9 @@ function ErrorCard(error: string): JSX.Element {
 		<div className="error-card">
 			<div className="error-content">
 				<h1>Something went wrong</h1>
-				<p>Error: {error}</p>
-				<button onClick={onDismiss} type="button">
+				<p className="text-red-800">{error}</p>
+				<p>The page will be reloaded automatically.</p>
+				<button onClick={dismiss} type="button">
 					I understand
 				</button>
 			</div>

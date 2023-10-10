a32d38e14042008599192278f6ad11549364be6a
Tim Bentum <tim_bentum@live.nl>
2023-10-10 16:52:36 +0200

    feat: :sparkles: Add copy button to code-block

--- src/components/fileView/CodeView.tsx
+++ src/components/fileView/CodeView.tsx
@@ -1,19 +1,37 @@
 import { codeExtensions } from '@/types/extensions';
 import { File } from '@/types/file';
 import CodeMirror from '@uiw/react-codemirror';
-import { Box } from '@chakra-ui/react';
+import { Box, Button, Flex } from '@chakra-ui/react';
 import { useModeColors } from '@/hooks/useColors';
-import { useMemo } from 'react';
+import { useMemo, useState } from 'react';
+import { CopyToClipboard } from 'react-copy-to-clipboard';
+import { CopyIcon } from '@chakra-ui/icons';
 
 export default function CodeView({ file }: { file: File }) {
 	const { codeMirror } = useModeColors();
+	const [copied, setCopied] = useState(false);
 
 	const content = useMemo(() => {
 		return Buffer.from(file.content, 'base64').toString('utf8');
 	}, [file]);
 
+	const handleCopy = () => {
+		setCopied(true);
+		setTimeout(() => {
+			setCopied(false);
+		}, 2000);
+	};
+
 	return (
-		<Box>
+		<Box className="pos-r">
+			<Flex alignItems="center" justifyContent="flex-end" mb={2}>
+				<CopyToClipboard onCopy={handleCopy} text={content}>
+					<Button className="copy-btn" colorScheme="green" mr={2} size="sm">
+						<CopyIcon mr={1} />
+						{copied ? 'Copied!' : 'Copy'}
+					</Button>
+				</CopyToClipboard>
+			</Flex>
 			<CodeMirror
 				editable={false}
 				extensions={codeExtensions[file.extension]}

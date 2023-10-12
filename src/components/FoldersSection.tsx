f205410bf7e9b52de2a531bb4d5eddffa7d75146
s1160928 <s1160928@student.windesheim.nl>
2023-10-12 20:43:49 +0200

    wip

--- src/components/FoldersSection.tsx
+++ src/components/FoldersSection.tsx
@@ -2,10 +2,11 @@ import { useModeColors } from '@/hooks/useColors';
 import { GitHubTreeItem } from '@/lib/repository/gitHubData';
 import { Card, CardBody } from '@chakra-ui/card';
 import { ChevronDownIcon } from '@chakra-ui/icons';
-import { Box, Collapse, useDisclosure } from '@chakra-ui/react';
+import { Box, Collapse, Input, InputGroup, InputLeftElement, useDisclosure } from '@chakra-ui/react';
 import Link from 'next/link';
 import Heading from './textStyles/Heading';
 import Text from './textStyles/Text';
+import { useState } from 'react';
 
 export const FoldersSection = ({
 	data,
@@ -16,6 +17,13 @@ export const FoldersSection = ({
 }) => {
 	const { isOpen, onToggle } = useDisclosure({ defaultIsOpen: true });
 	const rotate = isOpen ? 'rotate(-180deg)' : 'rotate(0)';
+
+	const [inputValue, setInputValue] = useState('');
+	const handleChange = (e) => {
+		setInputValue(e.target.value);
+	};	  
+	const filteredData = data ? data.filter((item) => item.name.toLowerCase().includes(inputValue.toLocaleLowerCase())) : [];
+
 	const { backgroundColorSecondary, backgroundColorPrimary, fontColor } =
 		useModeColors();
 
@@ -43,11 +51,19 @@ export const FoldersSection = ({
 					transition="all 0.2s linear"
 				/>
 			</Box>
+			<InputGroup>
+				<InputLeftElement
+					className="InputLeft"
+					pointerEvents="none"
+					children={<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#ffffff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><circle cx="11" cy="11" r="8"></circle><line x1="21" y1="21" x2="16.65" y2="16.65"></line></svg>}
+				/>
+				<Input onChange={handleChange} className="Input" variant="outline" placeholder="Search" />
+			</InputGroup>
 
 			{/* Content */}
 			<Collapse in={isOpen}>
 				<Box display="flex" gap={4} overflowX="auto" py={3}>
-					{data.map((item: GitHubTreeItem) => {
+					{filteredData.map((item: GitHubTreeItem) => {
 						return (
 							<Link
 								href={`/${encodeURIComponent(item.path)}`}
--- src/components/navigation/DesktopHeader.tsx
+++ src/components/navigation/DesktopHeader.tsx
@@ -1,12 +1,28 @@
-import { Box, Button, Show, Spacer, useColorMode } from '@chakra-ui/react';
+import { Box, Button, Show, Spacer, useColorMode, Input, InputGroup, InputLeftElement } from '@chakra-ui/react';
 import { Breadcrumbs } from '@/components/breadcrumbs/Breadcrumbs';
 import { MdDarkMode, MdLightMode } from 'react-icons/md';
+import { useState } from 'react';
+// import { FoldersSection } from '../FoldersSection';
 
 export function DesktopHeader() {
 	const { colorMode, toggleColorMode } = useColorMode();
+	// const [inputValue, setInputValue] = useState('');
+
+	// const handleChange = (e) => {
+	// 	setInputValue(e.target.value);
+	// };	  
 
 	return (
 		<Show above="md">
+			{/* <InputGroup>
+				<InputLeftElement
+					className="InputLeft"
+					pointerEvents="none"
+					children={<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#ffffff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><circle cx="11" cy="11" r="8"></circle><line x1="21" y1="21" x2="16.65" y2="16.65"></line></svg>}
+				/>
+				<Input onChange={handleChange} className="Input" variant="outline" placeholder="Search" />
+			</InputGroup> */}
+			{/* <FoldersSection filterValue={inputValue} /> */}
 			<Box p="2">
 				<Breadcrumbs />
 			</Box>

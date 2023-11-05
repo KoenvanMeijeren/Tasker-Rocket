49452d713a2b0072c4e09f915ea553be4990297a
FOXTetsuo <pownur@gmail.com>
2023-11-05 11:03:47 +0100

    errorboundary in app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,14 +2,17 @@ import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
 import { Providers } from '@/components/theme/providers';
 import SidebarWithHeader from '@/components/navigation/SidebarWithHeader';
+import ErrorBoundary from '@/components/error/ErrorBoundary';
 
 // eslint-disable-next-line @typescript-eslint/naming-convention
 export default function App({ Component, pageProps }: AppProps) {
 	return (
 		<Providers>
-			<SidebarWithHeader>
-				<Component {...pageProps} />
-			</SidebarWithHeader>
+			<ErrorBoundary>
+				<SidebarWithHeader>
+					<Component {...pageProps} />
+				</SidebarWithHeader>
+			</ErrorBoundary>
 		</Providers>
 	);
 }

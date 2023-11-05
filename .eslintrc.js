ba3e9e1916f5de9a4eb14c09347a33b593e16406
Tim Bentum <tim_bentum@live.nl>
2023-11-05 15:43:48 +0100

    style: :art: Eslint and prettier config update

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,9 @@
 module.exports = {
   env: {
-    "browser": true,
-    "es6": true
+    browser: true,
+    es6: true,
   },
-  ignorePatterns: [
-    "/*",
-    "!src/"
-  ],
+  ignorePatterns: ["/*", "!src/"],
   extends: [
     "eslint:recommended",
     "plugin:react/jsx-runtime",
@@ -19,42 +16,47 @@ module.exports = {
     "plugin:@next/next/recommended",
     "plugin:jsx-a11y/recommended",
     "plugin:prettier/recommended",
-    'plugin:etc/recommended',
+    "plugin:etc/recommended",
   ],
   // Specifying Parser
   parser: "@typescript-eslint/parser",
   parserOptions: {
     ecmaFeatures: {
-      jsx: true
+      jsx: true,
     },
     ecmaVersion: "latest",
     sourceType: "module",
     tsconfigRootDir: ".",
-    project: [
-      "./tsconfig.json"
-    ]
+    project: ["./tsconfig.json"],
   },
   // Configuring third-party plugins
-  plugins: [
-    "react",
-    "@typescript-eslint",
-    "filename-rules",
-    "react-refresh",
-    "etc",
-  ],
+  plugins: ["react", "@typescript-eslint", "filename-rules", "react-refresh", "etc"],
   // Resolve imports
   settings: {
     "import/resolver": {
       typescript: {
-        project: "./tsconfig.json"
-      }
+        project: "./tsconfig.json",
+      },
     },
     react: {
-      version: "18.x"
-    }
+      version: "18.x",
+    },
   },
   rules: {
-    "prettier/prettier": ["error", { useTabs: true, singleQuote: true, printWidth: 80,  endOfLine: "auto" }],
+    "prettier/prettier": [
+      "error",
+      {
+        useTabs: false,
+        singleQuote: true,
+        printWidth: 80,
+        endOfLine: "auto",
+        tabWidth: 4,
+        trailingComma: "es5",
+        bracketSpacing: true,
+        jsxBracketSameLine: false,
+        arrowParens: "always",
+      },
+    ],
     "react/prefer-stateless-function": "error",
     "react/button-has-type": "error",
     "react/no-unused-prop-types": "error",
@@ -65,16 +67,10 @@ module.exports = {
     "react/no-danger-with-children": "error",
     "react/no-unstable-nested-components": ["error", { allowAsProps: true }],
     "react/jsx-fragments": "error",
-    "react/destructuring-assignment": [
-      "error",
-      "always",
-      { destructureInSignature: "always" },
-    ],
+    "react/destructuring-assignment": ["error", "always", { destructureInSignature: "always" }],
     "react/jsx-no-leaked-render": ["error", { validStrategies: ["ternary"] }],
     "react/jsx-max-depth": ["error", { max: 6 }],
-    "react/function-component-definition": [
-      "off"
-    ],
+    "react/function-component-definition": ["off"],
     "react/jsx-key": [
       "error",
       {
@@ -126,15 +122,15 @@ module.exports = {
       },
       {
         selector: "enumMember",
-        format: ["PascalCase", "UPPER_CASE"]
-      }
+        format: ["PascalCase", "UPPER_CASE"],
+      },
     ],
     "@typescript-eslint/no-unsafe-call": "off",
-    "@typescript-eslint/no-unsafe-assignment":"off",
+    "@typescript-eslint/no-unsafe-assignment": "off",
     "import/no-default-export": "off",
-    'no-console': 'warn',
+    "no-console": "warn",
     "etc/no-t": "error",
-    'etc/no-commented-out-code': 'warn',
-    "jsx-a11y/media-has-caption": "off"
-  }
-}
+    "etc/no-commented-out-code": "warn",
+    "jsx-a11y/media-has-caption": "off",
+  },
+};

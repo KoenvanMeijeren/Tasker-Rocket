ae5f1128ed276c5841d4ccf032ba92c9b8f5a906
Tim Bentum <tim_bentum@live.nl>
2023-11-05 17:33:45 +0100

    feat: :sparkles: Add fullscreen mode to pdf viewer

--- src/components/fileView/FileContentView.tsx
+++ src/components/fileView/FileContentView.tsx
@@ -1,13 +1,24 @@
 import { useModeColors } from '@/hooks/useColors';
 import { useGitHubBlobContent } from '@/lib/repository/gitHubRepository';
 import {
-	removeFileExtension,
-	urlToFileExtension,
+    removeFileExtension,
+    urlToFileExtension,
 } from '@/lib/utility/formatters';
 import { FileType, findFileInfo } from '@/types/extensions';
-import { CheckCircleIcon, ChevronDownIcon } from '@chakra-ui/icons';
-import { Box, Collapse, Divider, Text, useDisclosure } from '@chakra-ui/react';
-import { useEffect, useMemo, useState } from 'react';
+import {
+    CheckCircleIcon,
+    ChevronDownIcon,
+    DownloadIcon,
+} from '@chakra-ui/icons';
+import {
+    Box,
+    Button,
+    Collapse,
+    Divider,
+    Text,
+    useDisclosure,
+} from '@chakra-ui/react';
+import { useCallback, useEffect, useMemo, useState } from 'react';
 import { colorConfig } from '../../../theme.config';
 import ImageView from './ImageView';
 import './fileContentView.css';
@@ -19,123 +30,189 @@ import AudioView from '@/components/fileView/AudioView';
 import VideoView from '@/components/fileView/VideoView';
 
 export default function FileContentView({
-	name,
-	contentUrl,
+    name,
+    contentUrl,
 }: {
-	name: string;
-	contentUrl: string;
+    name: string;
+    contentUrl: string;
 }) {
-	const { isOpen, onToggle } = useDisclosure();
-	const [file, setFile] = useState<File | undefined>(undefined);
+    const { isOpen, onToggle } = useDisclosure();
+    const [file, setFile] = useState<File | undefined>(undefined);
+    const [fileViewable, setFileViewable] = useState(true);
 
-	const rotate = isOpen ? 'rotate(-180deg)' : 'rotate(0)';
-	const { backgroundColorSecondary, fontColor, border } = useModeColors();
+    const rotate = isOpen ? 'rotate(-180deg)' : 'rotate(0)';
+    const { backgroundColorSecondary, fontColor, border } = useModeColors();
 
-	const { data, error, isLoading } = useGitHubBlobContent(contentUrl);
+    const { data, error, isLoading } = useGitHubBlobContent(contentUrl);
 
-	useEffect(() => {
-		if (!data) return;
+    useEffect(() => {
+        if (!data) return;
 
-		const extension = urlToFileExtension(name);
-		const fileInfo = findFileInfo(extension);
-		const itemName =
-			fileInfo.type === FileType.Markdown ? removeFileExtension(name) : name;
+        const extension = urlToFileExtension(name);
+        const fileInfo = findFileInfo(extension);
+        const itemName =
+            fileInfo.type === FileType.Markdown
+                ? removeFileExtension(name)
+                : name;
 
-		setFile({
-			name: itemName,
-			extension,
-			content: data,
-			fileType: fileInfo.type,
-			mimeType: fileInfo.mimeType,
-		});
-	}, [data, name]);
+        setFile({
+            name: itemName,
+            extension,
+            content: data,
+            fileType: fileInfo.type,
+            mimeType: fileInfo.mimeType,
+        });
+    }, [data, name]);
 
-	const content = useMemo(() => {
-		if (!file) return;
+    const handleDownload = useCallback(() => {
+        if (!file) return;
 
-		if (file.content.length < 1) {
-			return <>Dit bestand bevat geen content.</>;
-		}
+        const fileContent = new Blob([file.content], { type: file.mimeType });
+        const fileNameParts = file.name.split('.');
+        const fileExtension =
+            fileNameParts.length > 1 ? fileNameParts.pop() : '';
+        const fileNameWithoutExtension = fileNameParts.join('.');
+        const fileName =
+            fileNameWithoutExtension.endsWith(fileExtension ?? '') ||
+            !fileExtension
+                ? fileNameWithoutExtension
+                : `${fileNameWithoutExtension}.${fileExtension}`;
+        const link = document.createElement('a');
+        link.href = window.URL.createObjectURL(fileContent);
+        link.download = fileName;
+        link.click();
+    }, [file]);
 
-		switch (file.fileType) {
-			case FileType.Markdown:
-				return <MarkdownView file={file} />;
-			case FileType.Image:
-				return <ImageView file={file} />;
-			case FileType.Code:
-				return <CodeView file={file} />;
-			case FileType.Pdf:
-				return <PdfFileView file={file} />;
-			case FileType.Audio:
-				return <AudioView file={file} />;
-			case FileType.Video:
-				return <VideoView file={file} />;
-			case FileType.Docx:
-			case FileType.PowerPoint:
-			case FileType.Excel:
-			case FileType.Unsupported:
-				return <>De weergave van dit bestandstype wordt niet ondersteund.</>;
-		}
-	}, [file]);
+    const content = useMemo(() => {
+        if (!file) return;
 
-	if (error) {
-		return <div>laden mislukt...</div>;
-	}
+        if (file.content.length < 1) {
+            setFileViewable(false);
+            return (
+                <>
+                    Dit bestand is te groot om te bekijken.
+                    <Button
+                        className="btn btn-green"
+                        ml={3}
+                        onClick={handleDownload}
+                        size="sm"
+                    >
+                        <DownloadIcon mr={1} /> Download
+                    </Button>
+                </>
+            );
+        }
 
-	if (isLoading || !file) {
-		return null;
-	}
+        if (file.content.length < 1) {
+            return <>Dit bestand bevat geen content.</>;
+        }
 
-	return (
-		<Box
-			backgroundColor={backgroundColorSecondary}
-			borderRadius={8}
-			boxShadow="0px 4px 10px -3px rgba(0, 0, 0, 0.07)"
-			outline={isOpen ? `5px solid ${border}` : `0px solid ${border}`}
-			p={2}
-			transition="outline-width 200ms ease"
-			zIndex={2}
-		>
-			{/* Task header (collapsible) */}
-			<Box
-				alignItems="center"
-				cursor="pointer"
-				display="flex"
-				justifyContent="space-between"
-				onClick={onToggle}
-				px={4}
-			>
-				<Box alignItems="center" display="flex" gap="10px">
-					<CheckCircleIcon boxSize="20px" color={colorConfig.green} />
-					<Text className="noselect" fontSize="18px">
-						{file.name}
-					</Text>
-				</Box>
-				<Box>
-					<ChevronDownIcon
-						boxSize={10}
-						color={fontColor}
-						transform={rotate}
-						transition="all 0.2s linear"
-					/>
-				</Box>
-			</Box>
+        switch (file.fileType) {
+            case FileType.Markdown:
+                return <MarkdownView file={file} />;
+            case FileType.Image:
+                return <ImageView file={file} />;
+            case FileType.Code:
+                return <CodeView file={file} />;
+            case FileType.Pdf:
+                return <PdfFileView file={file} />;
+            case FileType.Audio:
+                return <AudioView file={file} />;
+            case FileType.Video:
+                return <VideoView file={file} />;
+            case FileType.Docx:
+            case FileType.PowerPoint:
+            case FileType.Excel:
+            case FileType.Unsupported:
+                setFileViewable(false);
+                return (
+                    <>
+                        De weergave van dit bestandstype wordt niet ondersteund.
+                        <Button
+                            className="btn btn-green"
+                            ml={3}
+                            onClick={handleDownload}
+                            size="sm"
+                        >
+                            <DownloadIcon mr={1} /> Download
+                        </Button>
+                    </>
+                );
+        }
+    }, [file, handleDownload]);
 
-			{/* Content */}
-			<Collapse in={isOpen}>
-				<Divider borderWidth={1.5} my={4} />
-				<Box px={4} py={4}>
-					<Box display="flex" justifyContent="flex-end" mb={6}>
-						<button className="btn btn-green" type="button">
-							<Box alignItems="center" display="flex" gap="8px">
-								<CheckCircleIcon color="white" />
-								<Text fontWeight="medium">Done</Text>
-							</Box>
-						</button>
-					</Box>
-					{content}
-				</Box>
-			</Collapse>
-		</Box>
-	);
+    if (error) {
+        return <div>laden mislukt...</div>;
+    }
+
+    if (isLoading || !file) {
+        return null;
+    }
+
+    return (
+        <Box
+            backgroundColor={backgroundColorSecondary}
+            borderRadius={8}
+            boxShadow="0px 4px 10px -3px rgba(0, 0, 0, 0.07)"
+            outline={isOpen ? `5px solid ${border}` : `0px solid ${border}`}
+            p={2}
+            transition="outline-width 200ms ease"
+            zIndex={2}
+        >
+            {/* Task header (collapsible) */}
+            <Box
+                alignItems="center"
+                cursor="pointer"
+                display="flex"
+                justifyContent="space-between"
+                onClick={onToggle}
+                px={4}
+            >
+                <Box alignItems="center" display="flex" gap="10px">
+                    <CheckCircleIcon boxSize="20px" color={colorConfig.green} />
+                    <Text className="noselect" fontSize="18px">
+                        {file.name}
+                    </Text>
+                </Box>
+                <Box>
+                    <ChevronDownIcon
+                        boxSize={10}
+                        color={fontColor}
+                        transform={rotate}
+                        transition="all 0.2s linear"
+                    />
+                </Box>
+            </Box>
+
+            {/* Content */}
+            <Collapse in={isOpen}>
+                <Divider borderWidth={1.5} my={4} />
+                <Box px={4} py={4}>
+                    <Box
+                        className="btn-group"
+                        display="flex"
+                        justifyContent="flex-end"
+                        mb={6}
+                    >
+                        {fileViewable ? (
+                            <Button
+                                className="btn btn-gray"
+                                onClick={handleDownload}
+                                size="sm"
+                            >
+                                <DownloadIcon />
+                            </Button>
+                        ) : null}
+                        <button className="btn btn-green" type="button">
+                            <Box alignItems="center" display="flex" gap="8px">
+                                <CheckCircleIcon color="white" />
+                                <Text fontWeight="medium">Done</Text>
+                            </Box>
+                        </button>
+                    </Box>
+                    {content}
+                </Box>
+            </Collapse>
+        </Box>
+    );
 }
--- src/components/fileView/PdfFileView.tsx
+++ src/components/fileView/PdfFileView.tsx
@@ -1,18 +1,54 @@
 import { File } from '@/types/file';
-import { useMemo } from 'react';
+import React, { useMemo } from 'react';
 import { blobFileToUrl } from '@/lib/utility/dataStructure';
+import { Button } from '@chakra-ui/react';
+import { FaExpandArrowsAlt } from 'react-icons/fa';
+import Text from '../textStyles/Text';
 
 export default function PdfFileView({ file }: { file: File }) {
-	const dataSrc = useMemo(() => {
-		return blobFileToUrl(file.content, file.mimeType);
-	}, [file]);
+    const viewerRef = React.createRef<HTMLDivElement>();
 
-	return (
-		<object data={dataSrc} height="600px" type={file.mimeType} width="100%">
-			<p>
-				Kan PDF-bestand niet weergeven. <a href={dataSrc}>Download</a> in plaats
-				daarvan.
-			</p>
-		</object>
-	);
+    const dataSrc = useMemo(() => {
+        return blobFileToUrl(file.content, file.mimeType);
+    }, [file]);
+
+    const enterFullScreen = async () => {
+        const viewer = viewerRef.current as HTMLDivElement;
+        if (viewer.requestFullscreen) {
+            viewer.requestFullscreen().catch((error) => {
+                console.log(error);
+            });
+        }
+    };
+
+    return (
+        <div>
+            <Button onClick={enterFullScreen} colorScheme="blue">
+                <FaExpandArrowsAlt color="white" />
+                <Text style={{ marginLeft: '5px' }} color="white">
+                    {' '}
+                    Full Screen
+                </Text>
+            </Button>
+            <div
+                ref={viewerRef}
+                style={{
+                    height: '600px',
+                    width: '100%',
+                }}
+            >
+                <object
+                    data={dataSrc}
+                    style={{ height: '100%', width: '100%' }}
+                    type={file.mimeType}
+                    width="100%"
+                >
+                    <p>
+                        Kan PDF-bestand niet weergeven.{' '}
+                        <a href={dataSrc}>Download</a> in plaats daarvan.
+                    </p>
+                </object>
+            </div>
+        </div>
+    );
 }

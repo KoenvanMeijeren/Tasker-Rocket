a94e6c6bfe5ecebf10c88d59961f4cc77ab26804
Tim Bentum <tim_bentum@live.nl>
2023-11-06 11:02:35 +0100

    feat: :sparkles: Add guard

--- src/components/fileView/ExcelView.tsx
+++ src/components/fileView/ExcelView.tsx
@@ -25,21 +25,21 @@ export default function ExcelView({ file }: { file: File }) {
     const [totalPages, setTotalPages] = useState(0);
 
     useEffect(() => {
-        if (file.mimeType.length > 0) {
-            const reader = new FileReader();
-            reader.readAsArrayBuffer(file.content);
-            reader.onloadend = (e) => {
-                const bufferArray = e.target?.result;
-                const wb = XLSX.read(bufferArray, { type: 'buffer' });
-                const wsname = wb.SheetNames[0];
-                const ws = wb.Sheets[wsname];
-                const data = XLSX.utils.sheet_to_json(ws);
-                // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-                //@ts-ignore
-                setExcelData(data);
-                setLoading(false);
-            };
-        }
+        if (file.mimeType.length <= 0) return;
+
+        const reader = new FileReader();
+        reader.readAsArrayBuffer(file.content);
+        reader.onloadend = (e) => {
+            const bufferArray = e.target?.result;
+            const wb = XLSX.read(bufferArray, { type: 'buffer' });
+            const wsname = wb.SheetNames[0];
+            const ws = wb.Sheets[wsname];
+            const data = XLSX.utils.sheet_to_json(ws);
+            // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+            //@ts-ignore
+            setExcelData(data);
+            setLoading(false);
+        };
     }, [file]);
 
     const filteredData = excelData

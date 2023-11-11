d8b400816bf0725310b5551da81ade2f4f0fc2b6
koenvanmeijeren <koenvanmeijeren@develto.nl>
2023-11-11 14:04:36 +0100

    chore: :construction: Enable strict mode

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,14 +2,17 @@ import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
 import { Providers } from '@/components/theme/providers';
 import SidebarWithHeader from '@/components/navigation/SidebarWithHeader';
+import { StrictMode } from 'react';
 
 // eslint-disable-next-line @typescript-eslint/naming-convention
 export default function App({ Component, pageProps }: AppProps) {
     return (
-        <Providers>
-            <SidebarWithHeader>
-                <Component {...pageProps} />
-            </SidebarWithHeader>
-        </Providers>
+        <StrictMode>
+            <Providers>
+                <SidebarWithHeader>
+                    <Component {...pageProps} />
+                </SidebarWithHeader>
+            </Providers>
+        </StrictMode>
     );
 }

0f9d7767da1827a3bb756c5be1d087bae182c02e
Fox <defoxnl@gmail.com>
2023-12-07 21:45:11 +0100

    added popup to delete page

--- src/pages/profileTabs/changeProfileInfo.tsx
+++ src/pages/profileTabs/changeProfileInfo.tsx
@@ -1,4 +1,4 @@
-import { Button, Heading, TabPanel, Text, Input } from '@chakra-ui/react';
+import { Button, Heading, TabPanel, Text, Input, VStack } from '@chakra-ui/react';
 import { useState } from 'react';
 
 export default function ChangeProfileInfoTab() {
@@ -13,21 +13,27 @@ export default function ChangeProfileInfoTab() {
 
     return (
         <TabPanel>
-            <Heading>Change Profile Info</Heading>
-            <Text>Update your first and last name:</Text>
-            <Input
-                placeholder="First Name"
-                value={firstName}
-                onChange={(e) => setFirstName(e.target.value)}
-            />
-            <Input
-                placeholder="Last Name"
-                value={lastName}
-                onChange={(e) => setLastName(e.target.value)}
-            />
-            <Button borderColor="blue" onClick={handleSaveChanges}>
-                Save Changes
-            </Button>
+            <VStack spacing={4}>
+                <Heading>Change Profile Info</Heading>
+                <Text>Update your first and last name:</Text>
+                <Input
+                    placeholder="First Name"
+                    value={firstName}
+                    onChange={(e) => setFirstName(e.target.value)}
+                />
+                <Input
+                    placeholder="Last Name"
+                    value={lastName}
+                    onChange={(e) => setLastName(e.target.value)}
+                />
+                <Button
+                    colorScheme="blue"
+                    onClick={handleSaveChanges}
+                    variant="ghost"
+                >
+                    Save Changes
+                </Button>
+            </VStack>
         </TabPanel>
     );
 }
--- src/pages/profileTabs/deleteAccount.tsx
+++ src/pages/profileTabs/deleteAccount.tsx
@@ -1,19 +1,66 @@
-import { useModeColors } from '@/hooks/useModeColors';
-import { Button, Heading, TabPanel, Text } from '@chakra-ui/react';
+import {
+    AlertDialog,
+    AlertDialogContent,
+    AlertDialogFooter,
+    AlertDialogHeader,
+    AlertDialogOverlay,
+    Button,
+    Heading,
+    TabPanel,
+    Text,
+    VStack,
+    useDisclosure,
+} from '@chakra-ui/react';
+import React from 'react';
 
 export default function DeleteAccountTab() {
-    const { backgroundColorPrimary } = useModeColors();
     const handleDeleteAccount = () => {
         // TODO: Implement delete account logic
     };
 
+    const { isOpen, onOpen, onClose } = useDisclosure();
+    const cancelRef = React.useRef<HTMLButtonElement | null>(null); // Add variable declarator
+
     return (
         <TabPanel>
-            <Heading>Delete Account</Heading>
-            <Text>Are you sure you want to delete your account?</Text>
-            <Button borderColor="red" onClick={handleDeleteAccount}>
-                Delete Account
-            </Button>
+            <AlertDialog
+                isOpen={isOpen}
+                leastDestructiveRef={cancelRef}
+                onClose={onClose}
+            >
+                <AlertDialogOverlay>
+                    <AlertDialogContent>
+                        <AlertDialogHeader fontSize="lg" fontWeight="bold">
+                            Are you sure?
+                        </AlertDialogHeader>
+
+                        <AlertDialogFooter>
+                            <Button onClick={onClose} ref={cancelRef}>
+                                Cancel
+                            </Button>
+                            <Button
+                                colorScheme="red"
+                                ml={3}
+                                onClick={handleDeleteAccount}
+                                variant="ghost"
+                            >
+                                Delete
+                            </Button>
+                        </AlertDialogFooter>
+                    </AlertDialogContent>
+                </AlertDialogOverlay>
+            </AlertDialog>
+
+            <VStack spacing={4}>
+                <Heading>Delete Account</Heading>
+                <Text>
+                    Deleting your account will remove all your data from the
+                    system. This action cannot be undone.
+                </Text>
+                <Button colorScheme="red" onClick={onOpen} variant="ghost">
+                    Delete Account
+                </Button>
+            </VStack>
         </TabPanel>
     );
 }

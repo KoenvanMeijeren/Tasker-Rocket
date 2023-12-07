26c99c51012b3a2330607bbbbc909b309fecc7ea
koenvanmeijeren <koenvanmeijeren@develto.nl>
2023-12-07 16:51:34 +0100

    chore: :construction: Enable strict mode for react

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,15 +4,18 @@ import { Providers } from '@/providers/Providers';
 import '@/styles/globals.css';
 import { Flex } from '@chakra-ui/layout';
 import type { AppProps } from 'next/app';
+import { StrictMode } from 'react';
 
 // eslint-disable-next-line @typescript-eslint/naming-convention
 export default function App({ Component, pageProps }: AppProps) {
     return (
-        <Providers>
-            <Flex height="100vh" overflow="hidden">
-                <SideBar />
-                <Content Component={Component} {...pageProps} />
-            </Flex>
-        </Providers>
+        <StrictMode>
+            <Providers>
+                <Flex height="100vh" overflow="hidden">
+                    <SideBar />
+                    <Content Component={Component} {...pageProps} />
+                </Flex>
+            </Providers>
+        </StrictMode>
     );
 }

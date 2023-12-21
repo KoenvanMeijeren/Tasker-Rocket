fac35a09c121a6b888799564e204a7f8d3ebf277
Kevin Fillet Cobos <KevinFilletCobos@gmail.com>
2023-12-21 15:39:01 +0100

    removed unnecessary code

--- cypress.config.ts
+++ cypress.config.ts
@@ -4,8 +4,5 @@ export default defineConfig({
     e2e: {
         supportFile: false,
         baseUrl: 'http://localhost:3000',
-        setupNodeEvents(on, config) {
-            // implement node event listeners here
-        },
     },
 });

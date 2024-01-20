60a220ac381706a76eddb39c55b6ae9c6cf51341
koenvanmeijeren <koenvanmeijeren@develto.nl>
2024-01-20 11:18:55 +0100

    bug: :ambulance: Only build the parent tree on allowed routes

--- src/lib/project/useParentTree.ts
+++ src/lib/project/useParentTree.ts
@@ -5,12 +5,15 @@ import { useCurrentPath } from '@/lib/utility/uri';
 import { MobxStore } from '@/lib/store/MobxStore';
 import { SessionContext } from '@/providers/SessionProvider';
 
+const allowedRoutes = ['/[...path]', '/'];
+
 export const useParentTree = (store: MobxStore) => {
-    const { path, isEmptyServerPath } = useCurrentPath();
+    const { path, pathname, isEmptyServerPath } = useCurrentPath();
     const sessionContext = useContext(SessionContext);
     const [parentTree, setParentTree] = useState<GitHubParentTree>();
 
     useEffect(() => {
+        if (!allowedRoutes.includes(pathname)) return;
         if (isEmptyServerPath || !sessionContext.session) return;
 
         const menuTree = store.menuTree.items;
@@ -19,7 +22,13 @@ export const useParentTree = (store: MobxStore) => {
             parent: result[0],
             tree: result,
         });
-    }, [isEmptyServerPath, path, sessionContext.session, store.menuTree.items]);
+    }, [
+        isEmptyServerPath,
+        path,
+        pathname,
+        sessionContext.session,
+        store.menuTree.items,
+    ]);
 
     return parentTree;
 };

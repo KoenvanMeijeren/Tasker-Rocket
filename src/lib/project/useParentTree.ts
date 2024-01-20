0e5b4adf7cab3690dc59a458ee7d2e0e6d3ebc49
koenvanmeijeren <koenvanmeijeren@develto.nl>
2024-01-20 10:21:19 +0100

    bug: :ambulance: Fix using the parent tree, when the user is not logged in

--- src/lib/project/useParentTree.ts
+++ src/lib/project/useParentTree.ts
@@ -1,15 +1,17 @@
-import { useEffect, useState } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import { GitHubParentTree } from '@/types/gitHubData';
 import { buildParentTreeForSearchPath } from '@/lib/utility/dataStructure';
 import { useCurrentPath } from '@/lib/utility/uri';
 import { MobxStore } from '@/lib/store/MobxStore';
+import { SessionContext } from '@/providers/SessionProvider';
 
 export const useParentTree = (store: MobxStore) => {
     const { path, isEmptyServerPath } = useCurrentPath();
+    const sessionContext = useContext(SessionContext);
     const [parentTree, setParentTree] = useState<GitHubParentTree>();
 
     useEffect(() => {
-        if (isEmptyServerPath) return;
+        if (isEmptyServerPath || !sessionContext.session) return;
 
         const menuTree = store.menuTree.items;
         const result = buildParentTreeForSearchPath(path, menuTree);
@@ -17,7 +19,7 @@ export const useParentTree = (store: MobxStore) => {
             parent: result[0],
             tree: result,
         });
-    }, [isEmptyServerPath, path, store.menuTree.items]);
+    }, [isEmptyServerPath, path, sessionContext.session, store.menuTree.items]);
 
     return parentTree;
 };

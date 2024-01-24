026a88b79ed0f1aa4c069488fc32ecb3840c430c
Tim Bentum <tim_bentum@live.nl>
2024-01-24 12:33:13 +0100

    fix: :bug: Route to / before changeing repo

--- src/lib/navigation/useRepoSwitchHandler.tsx
+++ src/lib/navigation/useRepoSwitchHandler.tsx
@@ -1,13 +1,20 @@
+import { useRouter } from 'next/router';
 import { MobxStore } from '../store/MobxStore';
 
 export const useRepoSwitchHandler = (store: MobxStore) => {
+    const router = useRouter();
     const changeSelectedItem = (item: string) => {
-        const repoItem = store.repositoryConfig.items.find(
-            (repo) => repo.repository === item
-        );
-        if (!repoItem) return;
-        store.gitHubItemsState.initRepository(repoItem.repository);
-        store.repositoryConfig.setSelectedItem(repoItem);
+        router
+            .push('/')
+            .then(() => {
+                const repoItem = store.repositoryConfig.items.find(
+                    (repo) => repo.repository === item
+                );
+                if (!repoItem) return;
+                store.gitHubItemsState.initRepository(repoItem.repository);
+                store.repositoryConfig.setSelectedItem(repoItem);
+            })
+            .catch((e) => console.error(e));
     };
 
     return {

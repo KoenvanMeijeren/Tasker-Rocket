cb37c11e85fc08b43b4775452ad10c8f59af0cc8
Tim Bentum <tim_bentum@live.nl>
2024-01-24 11:35:02 +0100

    feat: :sparkles: Hide selector when loggedout

--- src/components/navigation/Header.tsx
+++ src/components/navigation/Header.tsx
@@ -1,6 +1,5 @@
 import { useColorConfig } from '@/lib/colors/useColorConfig';
 import { useStore } from '@/lib/store';
-import { RepositoryConfigItem } from '@/lib/store/slices/RepositoryConfigStore';
 import { Box, Flex, Spacer } from '@chakra-ui/layout';
 import {
     Button,
@@ -10,15 +9,19 @@ import {
     useColorModeValue,
 } from '@chakra-ui/react';
 import { observer } from 'mobx-react-lite';
-import { useEffect, useState } from 'react';
 import { MdDarkMode, MdLightMode } from 'react-icons/md';
 import { Breadcrumbs } from '../breadcrumbs/Breadcrumbs';
 import UserProfileCard from '../userProfile/UserProfileCard';
+
+import { SessionContext } from '@/providers/SessionProvider';
+import { useContext } from 'react';
 
 const Header = observer(() => {
     const store = useStore();
     const { colorMode, toggleColorMode } = useColorMode();
     const colorConfig = useColorConfig();
+
+    const { session } = useContext(SessionContext);
 
     const boxShadow = useColorModeValue(
         '0px -5px 10px rgba(0,0,0,0.5)',
@@ -51,17 +54,23 @@ const Header = observer(() => {
                     <Breadcrumbs />
                 </Box>
                 <Spacer />
-                <Select
-                    onChange={(e) => changeSelectedItem(e.target.value)}
-                    value={store.repositoryConfig.selectedItem?.repository}
-                    width={200}
-                >
-                    {store.repositoryConfig.items.map((repo) => (
-                        <option key={repo.repository} value={repo.repository}>
-                            {repo.repository}
-                        </option>
-                    ))}
-                </Select>
+                {/* if session display the select */}
+                {session ? (
+                    <Select
+                        onChange={(e) => changeSelectedItem(e.target.value)}
+                        value={store.repositoryConfig.selectedItem?.repository}
+                        width={200}
+                    >
+                        {store.repositoryConfig.items.map((repo) => (
+                            <option
+                                key={repo.repository}
+                                value={repo.repository}
+                            >
+                                {repo.repository}
+                            </option>
+                        ))}
+                    </Select>
+                ) : null}
                 <Button onClick={toggleColorMode}>
                     {colorMode === 'light' ? (
                         <MdDarkMode color="black" />
